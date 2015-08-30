b8bc84f736d3cfdcd386fc4b29b6908577554b70
xmzb1218 <xmzb1218@users.noreply.github.com>
2015-08-30 23:45:31 +0800

    可正常工作的WebAPP框架

--- www/app.py
+++ www/app.py
@@ -120,7 +120,6 @@ def init(loop):
 		logger_factory, response_factory
 	])
 	init_jinja2(app, filters = dict(datetime = datetime_filter))
-	app.router.add_route('GET', '/', index)
 	add_routes(app, 'handlers')
 	add_static(app)
 	srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
